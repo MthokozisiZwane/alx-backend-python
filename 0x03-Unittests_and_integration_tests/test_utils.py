8ba67607459e7d50c0aa3637363f5105f27a6f28
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-04-12 14:40:55 -0700

    . Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,8 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, MagicMock, PropertyMock
+from utils import get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +32,25 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), expected_message)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test case for the get_json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test get_json function with mocked HTTP calls."""
+
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
