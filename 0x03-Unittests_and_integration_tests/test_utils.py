861e0de114615185d5e8b4a3afe0aaa6a2e5ccbe
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-04-12 15:44:41 -0700

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -53,5 +53,44 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, test_payload)
 
 
+def memoize(fn):
+    attr_name = "_{}".format(fn.__name__)
+
+    def memoized(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test memoize decorator with a method."""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+
+#        with patch.object(TestClass, 'a_method') as mock_method:
+
+        result1 = test_instance.a_property
+        result2 = test_instance.a_property
+
+        # Asserts that the method was called only once
+        # mock_method.assert_called_once()
+        # Asserts that both results are as expected
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
+
+
 if __name__ == '__main__':
     unittest.main()
